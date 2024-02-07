19a3efdeeeb440310bf85cb212f051077d481eac
Balogh Levente <baloghm.levente@gmail.com>
2024-02-07 14:25:21 +0100

    second commit

--- src/app.ts
+++ src/app.ts
@@ -1,37 +1,51 @@
-import express, {Request, Response, NextFunction} from 'express';
-import IController from './interfaces/controller.interface' 
- 
+import express, { Request, Response, NextFunction } from "express";
+import IController from "./interfaces/controller.interface";
+import { config } from "dotenv";
+import mongoose from "mongoose";
+
 export default class App {
   public app: express.Application;
-  public port: number;
- 
-  constructor(controllers: IController[], port: number) {
+
+  constructor(controllers: IController[]) {
+    config();
     this.app = express();
-    this.port = port;
- 
-    this.initializeMiddlewares();
-    this.initializeControllers(controllers);
+    this.connectToDatabase(controllers);
   }
 
-  loggerMiddleware(request: Request, response: Response, next: NextFunction) {
+  private loggerMiddleware(
+    request: Request,
+    response: Response,
+    next: NextFunction
+  ) {
     console.log(`${request.method} ${request.path}`);
     next();
   }
- 
+
   private initializeMiddlewares() {
     this.app.use(express.json());
     this.app.use(this.loggerMiddleware);
   }
- 
+
   private initializeControllers(controllers: IController[]) {
     controllers.forEach((controller) => {
-      this.app.use('/', controller.router);
+      this.app.use("/", controller.router);
     });
   }
- 
-  public listen() {
-    this.app.listen(this.port, () => {
-      console.log(`App listening on the port ${this.port}`);
+
+  public connectToDatabase(controllers: IController[]) {
+    const { MONGO_URI, MONGO_DB, PORT } = process.env;
+    mongoose.connect(MONGO_URI as string, { dbName: MONGO_DB});
+
+    mongoose.connection.on("connected", () => {
+      console.log("Connected to MongoDB server.");
+      this.initializeMiddlewares();
+      this.initializeControllers(controllers);
+      this.app.listen(PORT, () => {
+        console.log(`App listening on the port ${PORT}`);
+      });
+    });
+    mongoose.connection.on("error", error => {
+      console.log(`Mongoose error: ${error.message}`)
     });
   }
 }
